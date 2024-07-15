4db82dad1501038b0369e61cecf041258431920e
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-15 16:45:03 +0300

    not-found page bug

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import star from '../assets/not-found.png';
 
-export default function Page404() {
+export default function NotFound() {
   return (
     <div className="w-[90vw] h-[80vh] flex justify-center items-center gap-12">
       <Image src={star} width={200} height={200} alt="Death star" />
