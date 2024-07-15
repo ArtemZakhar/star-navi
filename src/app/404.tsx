5de2376a69cca7b7cd2877ff307aa882501f01a6
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-15 16:28:09 +0300

    not-found page bug

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import star from '../assets/not-found.png';
 
-export default function NotFound() {
+export default function Page404() {
   return (
     <div className="w-[90vw] h-[80vh] flex justify-center items-center gap-12">
       <Image src={star} width={200} height={200} alt="Death star" />
-      <p className='text-yellow text-2xl'>
-        <span className='line-through'>Planet</span> Page not found
+      <p className="text-yellow text-2xl">
+        <span className="line-through">Planet</span> Page not found
       </p>
     </div>
   );
